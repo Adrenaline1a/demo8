d58dc9964b4e889ecb9ebdbc52019869673eb1cc
Not a Hero <78856293+Adrenaline1a@users.noreply.github.com>
2021-09-12 15:22:11 +0300

    last

--- ind.py
+++ ind.py
@@ -1,9 +1,9 @@
 flot = []
 while True:
     com = input('Введите команду: ').lower()
-    if com == 'Выход':
+    if com == 'exit':
         break
-    elif com == "Добавить":
+    elif com == "add":
         tip = input('Введите тип самолёта: ')
         nomer = input('Введите номер самолёта: ')
         mesto = input('Введите место прибытия: ')
@@ -15,17 +15,15 @@ while True:
         flot.append(aflot)
         if len(flot) > 1:
             flot.sort(key=lambda x: x.get('mesto', ''))
-    elif com == 'Список':
+    elif com == 'list':
         for ind, reys in enumerate(flot, 1):
-            print(ind, 'Место прибытия: ',
-                    reys.get('mesto', ''), "\n",
-                  'Номер: ',
-                    reys.get('nomer', ''), "\n",
-                  'Тип: ',
-                    reys.get('tip', ''))
-    elif com == 'Выбрать':
+            print(ind, 'Место прибытия: ', reys.get('mesto', ''), "\n",
+                  'Номер: ', reys.get('nomer', ''), "\n", 'Тип: ',
+                  reys.get('tip', ''))
+    elif com == 'select':
         nom = input('Введите тип желаемого самолёта: ')
         for i, num in enumerate(flot, 1):
             if nom == num.get('tip', ''):
-                print("Тип: ", num.get('tip', ''), "\n", "Номер: ", num.get('nomer', ''), "\n", "Место прибытия: ",
-                      num.get('mesto', ''))
+                print("Тип: ", num.get('tip', ''), "\n", "Номер: ",
+                      num.get('nomer', ''),
+                      "\n", "Место прибытия: ", num.get('mesto', ''))
